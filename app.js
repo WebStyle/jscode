037e4ac46b71eff999f4cc0df3a0e102ce4853ca
Farrukh Mamatkhalilov <faxa007@gmail.com>
2015-10-02 08:25:52 +0500

    add comments

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+/**
+ * JavaScript shablonlarni chaqirish.
+ * Function vs Method.
+ */
+
+/**
+ * Model
+ */
 var model = (function() {
 
+  /**
+   * Function
+   */
 	var funcPrivate = function() {
     return 'this is private function';
 	};
 
+  /**
+   * Method
+   */
 	return {
 		summa: function(a, b) {
 			return a + b
-		},
-    funcPublic: funcPrivate
+		}
 	}
 })();
 
+/**
+ * with context
+ */
 var arr = [4,5];
 var add = function (a, b) {
   console.log(this.summa(20,2));
